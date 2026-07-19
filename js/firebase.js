0584fe8b657604cadba91f6d2708962b23eaee49
abdulkader5081028-apk <abdulkader5081028@gmail.com>
2026-07-20 00:09:20 +0600

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,18 @@
+// js/firebase.js
+// Firebase SDK modules import (CDN version)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+// ফায়ারবেস কনফিগারেশন (আমরা পরে Firebase Console থেকে আসল কীগুলো এখানে বসাবো)
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
