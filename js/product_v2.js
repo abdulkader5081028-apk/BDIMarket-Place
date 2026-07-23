bd52fe6e871d7244abd9b6df8ec161015146da0f
abdulkader5081028-apk <abdulkader5081028@gmail.com>
2026-07-24 01:27:16 +0600

    Update product_v2.js

--- js/product_v2.js
+++ js/product_v2.js
@@ -1,1 +1,89 @@
+import { db } from "./firebase.js";
 
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+// ======================
+// Get Product ID
+// ======================
+
+const params = new URLSearchParams(window.location.search);
+
+const productId = params.get("id");
+
+// ======================
+// HTML Elements
+// ======================
+
+const productImage = document.getElementById("productImage");
+
+const productName = document.getElementById("productName");
+
+const productPrice = document.getElementById("productPrice");
+
+const productCategory = document.getElementById("productCategory");
+
+const productDescription = document.getElementById("productDescription");
+
+const productStock = document.getElementById("productStock");
+
+// ======================
+// Load Product
+// ======================
+
+async function loadProduct() {
+
+    if (!productId) {
+
+        alert("Product not found.");
+
+        return;
+
+    }
+
+    try {
+
+        const productRef = doc(db, "products", productId);
+
+        const productSnap = await getDoc(productRef);
+
+        if (!productSnap.exists()) {
+
+            alert("Product not found.");
+
+            return;
+
+        }
+
+        const product = productSnap.data();
+
+        productImage.src = product.image;
+
+        productName.textContent = product.name;
+
+        productPrice.textContent = "$" + product.price;
+
+        productCategory.textContent = product.category;
+
+        productDescription.textContent = product.description;
+
+        productStock.textContent =
+            product.stock > 0
+                ? "✅ In Stock"
+                : "❌ Out of Stock";
+
+    }
+
+    catch (error) {
+
+        console.error(error);
+
+        alert("Failed to load product.");
+
+    }
+
+}
+
+loadProduct();
